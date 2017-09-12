15f5f7d5435a05576e0c595392a76d2e1f378102
Daniele Gallingani <daniele@buildo.io>
2017-09-12 19:11:10 +0200

    update spray-autoproductformat version (spray 1.3.2)

--- jsend/build.sbt
+++ jsend/build.sbt
@@ -1,9 +1,9 @@
 name := "ingredients-jsend"
 
-version       := "0.4.1-SNAPSHOT"
+version       := "0.5"
 
 libraryDependencies ++= Seq(
-  "io.buildo"      %% "spray-autoproductformat" % "0.2",
+  "io.buildo"      %% "spray-autoproductformat" % "0.4.0",
   "org.scalatest"  %% "scalatest"     % "2.2.0" % "test",
   "org.mockito"    %  "mockito-all"   % "1.9.5" % "test"
 )
--- jsend/src/main/scala/io.buildo.ingredients/jsend/dsl.scala
+++ jsend/src/main/scala/io.buildo.ingredients/jsend/dsl.scala
@@ -1,17 +1,17 @@
 package ingredients.jsend
 
-import spray.json.AutoProductFormattable
+import spray.json.AutoProductFormat
 
 package object dsl {
-  def `for`[T <: Product] = new AutoProductFormattable[T] {
-    def serializeOneAs(singularNameP: String) = new AutoProductFormattable[T] with JSendSingular[T] {
+  def `for`[T <: Product] = new AutoProductFormat {
+    def serializeOneAs(singularNameP: String) = new AutoProductFormat with JSendSingular[T] {
       val singularName = singularNameP
-      def andMultipleAs(pluralNameP: String) = new AutoProductFormattable[T] with JSendSingular[T] with JSendPlural[T] {
+      def andMultipleAs(pluralNameP: String) = new AutoProductFormat with JSendSingular[T] with JSendPlural[T] {
         val singularName = singularNameP
         val pluralName = pluralNameP
       }
     }
-    def serializeMultipleAs(pluralNameP: String) = new AutoProductFormattable[T] with JSendPlural[T] {
+    def serializeMultipleAs(pluralNameP: String) = new AutoProductFormat with JSendPlural[T] {
       val pluralName = pluralNameP
     }
   }
